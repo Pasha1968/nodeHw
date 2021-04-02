8ee7a0b33b833a051cd073d56e6e1a79a7211cdc
Porti <Pasha19682000d@gmail.com>
2021-04-02 21:01:28 +0300

    it works

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 const express = require("express");
 const fs = require("fs");
-    
+const dirTree = require("directory-tree");
 const app = express();
 const jsonParser = express.json();
   
 app.use(express.static(__dirname + "/public"));
   
-app.get("/:path", function(req, res){
-       
-    const path = req.params.path;
-    fs.readdir(path, (err, files) => {
-      files.forEach(file => {
-        console.log(file);
-      });
-    });
+// app.get("/:path", function(req, res){
+app.get("/", function(req, res){
+    var responce
+   // const path = req.params.path;
+    const path = req.param('path');
+    const tree = dirTree(path);
+    if (tree.type == "file") {
+        console.log("file chel");
+        res.send(
+            tree.path
+        )
+    }
+    res.send({
+        tree
+    })
+    console.log(path);
+    // fs.readdir(path, (err, files) => {
+    //   files.forEach(file => {
+    //     console.log(file);
+    //     responce = file;
+    //   });
+    // });
+    // console.log(responce);
+    // res.send({
+    //     responce,
+    // })
 });
-// получение одного пользователя по id
 
    
 async function start(){
